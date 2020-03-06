c81cb634860fc60ac5d77dab4b448a11220b08fa
orangstr <majin.byanjankar@gmail.com>
2020-03-06 00:49:17 -0600

    without keys

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <form action="{{ route('create-payment') }}" method="post">
+    @csrf 
+    <input type="submit" value="Pay Now">
+    </form>
+    </div>
+</body>
+</html>
