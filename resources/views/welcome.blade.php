39ca1d734bd279a0ddb6365e769b370d929cbaeb
orangstr <majin.byanjankar@gmail.com>
2020-03-15 07:15:35 -0500

    branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,3 +15,25 @@
     </div>
 </body>
 </html>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<link href="agecheck.css" rel="stylesheet">
+<script src="jquery.agecheck.min.js"></script>
+
+$.ageCheck({
+  minAge: 21,
+  redirectTo: '',
+  redirectOnFail: '',
+  title: 'Age Verification',
+  copy: 'This Website requires you to be [21] years or older to enter. Please enter your Date of Birth in the fields below in order to continue:',
+  successMsg: {
+    header: 'Success!',
+    body: 'You are now being redirected back to the application...'
+  },
+  underAgeMsg: 'Sorry, you are not old enough to view this site...',
+  errorMsg: {
+    invalidDay: 'Day is invalid or empty',
+    invalidYear: 'Year is invalid or empty'
+  }
+});
